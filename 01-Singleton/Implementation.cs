011fd174993fa1ff68344cfa19dd14241b91d0bd
atemengue <moafembe@gmail.com>
2024-12-01 15:18:22 +0100

    Factory Method

--- 01-Singleton/Implementation.cs
+++ 01-Singleton/Implementation.cs
@@ -1,10 +1,16 @@
 ﻿namespace Singleton
 {
+    /// <summary>
+    /// Singleton
+    /// </summary>
     public class Logger
     {
 
         private static Logger? _instance;
 
+        /// <summary>
+        /// Instance
+        /// </summary>
         public static Logger? Instance
         {
             get
@@ -16,8 +22,15 @@
                 return _instance;
             }
         }
+        /// <summary>
+        /// Constructor
+        /// </summary>
         protected Logger() { }
 
+        /// <summary>
+        /// Singleton Operation
+        /// </summary>
+        /// <param name="message"></param>
         public void Log(string message)
         {
             Console.WriteLine($"Message to log: {message}");
